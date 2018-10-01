b923606e1ea2f28365859f2b933159dfcfe0b66d
Ryder T Swan <oars@bartswan.com>
2018-10-01 18:00:28 -0500

    Arm Kinematics and hardware class
    Created arm tracking and hardware class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/armKinematics.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode;
+
+/**
+ * Created by ryderswan on 10/1/18.
+ *
+ * Calculate the arm position in three dimensional space
+ */
+
+public class armKinematics extends robotHardware {
+
+    //Declare all variables
+    int armPosX = 0;
+    int armPosy = 0;
+    int armPosz = 0;
+
+
+    //Code
+
+
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotHardware.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode;
+
+/**
+ * Created by ryderswan on 10/1/18.
+ */
+
+public class robotHardware {
+
+    // Declare all variables
+
+    int armSegmentsNumber = 3;
+    int armSegmentsLength = 10;
+
+}
