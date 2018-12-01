73d1e1e45f0757ea40ce57a69ad4d3f83ec42f6c
Ashley Trowell <ajtrowell@gmail.com>
2018-12-01 03:01:27 -0600

    Calibrated MecanumNav after removing wheel slop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utilities/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utilities/Constants.java
@@ -28,10 +28,10 @@ public class Constants {
     public static final double LIFTER_MAX_ANGEL = 0;
 
     public static final double DRIVE_WHEEL_DIAMETER_INCHES  =  6/1.057;
-    public static final double DRIVE_WHEEL_LATERAL_RATIO = 0.89 * 0.963 / 0.95884;
+    public static final double DRIVE_WHEEL_LATERAL_RATIO = 0.89 * 0.963 / 0.95884 * 0.96312;
 
     public static final double MM_PER_IN = 25.4f;
-    private static double rotationScaleIncrease = 1.0975 / 1.088844444;
+    private static double rotationScaleIncrease = 1.0975 / 1.088844444; //1.0084722;
     public static final double WHEELBASE_WIDTH_IN = 15 / rotationScaleIncrease;
     public static final double WHEELBASE_LENGTH_IN = 11.75 / rotationScaleIncrease;
     public static final double WHEELBASE_WIDTH_MM  = WHEELBASE_WIDTH_IN  * MM_PER_IN;
